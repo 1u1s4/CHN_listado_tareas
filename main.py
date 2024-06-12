4b00b30247f725115cce1c0d2a9f4ad2a579771f
Luis Alvarado <3553751921101@ingenieria.usac.edu.gt>
2024-06-12 10:45:04 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,25 +2,32 @@ import pandas as pd
 from typing import Optional
 import datetime as dt
 
-def filtrar_tareas(df, mes: int, año: int, nombre: Optional[str] = None, descripcion: bool = False, excluir_email: bool = False):
+def filtrar_tareas(df, fecha_inicial: Optional[str] = None, fecha_final: Optional[str] = None, nombre: Optional[str] = None, descripcion: bool = True, excluir_email: bool = True):
     """
-    Filtra las tareas en el dataframe basado en el mes, año, y opcionalmente por un nombre específico.
+    Filtra las tareas en el dataframe basado en una fecha inicial y una fecha final, y opcionalmente por un nombre específico.
     Si se activa excluir_email, las tareas con descripciones que comienzan con "Email:" son excluidas.
     La columna 'Fecha límite' se utiliza para filtrar por fecha.
 
     :param df: DataFrame de tareas.
-    :param mes: Mes para filtrar.
-    :param año: Año para filtrar.
+    :param fecha_inicial: Fecha inicial para filtrar en formato 'YYYY-MM-DD'.
+    :param fecha_final: Fecha final para filtrar en formato 'YYYY-MM-DD'.
     :param nombre: Nombre opcional para filtrar en 'Responsable' o 'Participantes'.
     :param descripcion: Si True, incluye la descripción en la salida.
     :param excluir_email: Si True, excluye tareas con descripciones que empiezan por "Email:".
     :return: String formateado con las tareas filtradas.
     """
 
-    # Filtrar por mes y año
-    df_filtrado = df[df['Fecha límite'].dt.month == mes]
-    df_filtrado = df_filtrado[df_filtrado['Fecha límite'].dt.year == año]
+    # Establecer fechas por defecto si no se proporcionan
+    if not fecha_inicial:
+        fecha_inicial = (dt.datetime.now() - dt.timedelta(days=30)).strftime('%Y-%m-%d')
+    if not fecha_final:
+        fecha_final = dt.datetime.now().strftime('%Y-%m-%d')
 
+    fecha_inicial = pd.to_datetime(fecha_inicial)
+    fecha_final = pd.to_datetime(fecha_final)
+
+    # Filtrar por rango de fechas
+    df_filtrado = df[(df['Fecha límite'] >= fecha_inicial) & (df['Fecha límite'] <= fecha_final)]
     # Filtrar por nombre si se proporciona
     if nombre:
         df_filtrado = df_filtrado[(df_filtrado['Responsable'].str.contains(nombre, na=False, case=False)) | 
@@ -45,7 +52,7 @@ if __name__ == '__main__':
     tareas_df = pd.read_excel('tareas.xlsx')
     tareas_df['Fecha límite'] = pd.to_datetime(tareas_df['Fecha límite'], dayfirst=True, format='%d/%m/%Y %H:%M:%S')
 
-    ejemplo_salida = filtrar_tareas(tareas_df, 4, 2024, nombre='Luis Alvarado', descripcion=True, excluir_email=True)
+    ejemplo_salida = filtrar_tareas(tareas_df, fecha_inicial='2024-05-16', fecha_final='2024-06-12')
 
     with open('tareas.txt', 'w', encoding='utf-8') as f:
         f.write(ejemplo_salida)
